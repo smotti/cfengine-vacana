b487e5b5f16b57126b75cfe62ed328b96dae71d3
otti <otti@gargamels-haus.net>
2015-03-28 18:26:27 +0800

    Add some documents

---
+++ doc/Vacana.mm
@@ -0,0 +1,189 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1427511194431" ID="ID_1392795463" MODIFIED="1427511239140" TEXT="Vacana">
+<font BOLD="true" NAME="SansSerif" SIZE="17"/>
+<node CREATED="1427511211796" FOLDED="true" ID="ID_1176035889" MODIFIED="1427514377281" POSITION="right" TEXT="What">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="help"/>
+<icon BUILTIN="password"/>
+<node CREATED="1427511280546" ID="ID_1267062980" MODIFIED="1427511312313" TEXT="Up to date and simple CFEngine library that provides methods for the most common task/promises"/>
+<node CREATED="1427511316454" ID="ID_989193908" MODIFIED="1427511348732" TEXT="A web app that gives an overview over the hosts managed by cfengine with an inventory each one of them and their promise outcomes">
+<node CREATED="1427511350669" ID="ID_845375109" MODIFIED="1427511353595" TEXT="Classes also"/>
+</node>
+<node CREATED="1427512713415" ID="ID_1178069998" MODIFIED="1427512723472" TEXT="Based on CFEngine-Community"/>
+</node>
+<node CREATED="1427511356677" FOLDED="true" ID="ID_804309881" MODIFIED="1427513624698" POSITION="left" TEXT="Why">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="help"/>
+<node CREATED="1427511379107" ID="ID_1488239982" MODIFIED="1427511451337" TEXT="NCF">
+<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node CREATED="1427511439037" ID="ID_1598645717" MODIFIED="1427511474046" TEXT="PRO">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node CREATED="1427511543105" ID="ID_629656899" MODIFIED="1427511589097" TEXT="Bundles written in CFEngine"/>
+<node CREATED="1427511664318" ID="ID_752109165" MODIFIED="1427511672029" TEXT="Simple and short outcome classes"/>
+</node>
+<node CREATED="1427511479401" ID="ID_946092917" MODIFIED="1427511483720" TEXT="CON">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node CREATED="1427511369626" ID="ID_1165500503" MODIFIED="1427511377698" TEXT="NCF is not kept up to date"/>
+<node CREATED="1427511414764" ID="ID_764297620" MODIFIED="1427511418404" TEXT="Not beginner friendly"/>
+<node CREATED="1427511390197" ID="ID_91634633" MODIFIED="1427511397887" TEXT="To much around it"/>
+<node CREATED="1427511657186" ID="ID_379433957" MODIFIED="1427511662696" TEXT="No reporting"/>
+</node>
+</node>
+<node CREATED="1427511399759" ID="ID_73845931" MODIFIED="1427511457155" TEXT="EFL">
+<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node CREATED="1427511463754" ID="ID_381851291" MODIFIED="1427511475591" TEXT="PRO">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node CREATED="1427511493138" ID="ID_914489131" MODIFIED="1427511495635" TEXT="Data driven"/>
+<node CREATED="1427511648680" ID="ID_191233390" MODIFIED="1427511654397" TEXT="Includes reporting"/>
+</node>
+<node CREATED="1427511479401" ID="ID_1080310693" MODIFIED="1427511483720" TEXT="CON">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node CREATED="1427511496849" ID="ID_1959603906" MODIFIED="1427511505132" TEXT="JSON feels very verbose and repetitive"/>
+<node CREATED="1427511427891" ID="ID_1696569371" MODIFIED="1427511436860" TEXT="Some common bundles a missing"/>
+<node CREATED="1427511419992" ID="ID_664180865" MODIFIED="1427511426954" TEXT="Also not really beginner friendly"/>
+<node CREATED="1427511674283" ID="ID_1215705809" MODIFIED="1427511679469" TEXT="Outcome classes are to long"/>
+<node CREATED="1427512509762" ID="ID_962860088" MODIFIED="1427512515109" TEXT="Lack of documentation"/>
+</node>
+</node>
+<node CREATED="1427511594408" ID="ID_1306775528" MODIFIED="1427511611960" TEXT="Personal">
+<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node CREATED="1427511613017" ID="ID_422839085" MODIFIED="1427511629165" TEXT="Like to be close to cfengine, that means write policies within its language"/>
+<node CREATED="1427511630155" ID="ID_227246184" MODIFIED="1427511633182" TEXT="Flexibility"/>
+<node CREATED="1427512374428" ID="ID_1493821087" MODIFIED="1427512421194" TEXT="Like to see a better lightweight, simple to setup and easy to use dashboard/webapp">
+<node CREATED="1427512424510" ID="ID_1006298995" MODIFIED="1427512460222" TEXT="Rudder is to heavy, for bigger setups and compliance oriented"/>
+<node CREATED="1427512465290" ID="ID_637597557" MODIFIED="1427512482412" TEXT="Rudder also takes away the need to write stuff in cfengine (more or less)"/>
+<node CREATED="1427512484984" ID="ID_189690424" MODIFIED="1427512500877" TEXT="Delta Reporting feels old and is not really easy to install"/>
+<node CREATED="1427512518354" ID="ID_1210544889" MODIFIED="1427512527618" TEXT="Delta reporting also lacks documentation"/>
+</node>
+</node>
+</node>
+<node CREATED="1427512607707" FOLDED="true" ID="ID_997629443" MODIFIED="1427514376812" POSITION="right" TEXT="How">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="help"/>
+<icon BUILTIN="password"/>
+<node CREATED="1427512628380" ID="ID_1003209890" MODIFIED="1427512662325" TEXT="Parameterized bundles that can be called via methods promises">
+<node CREATED="1427512665033" ID="ID_1728854868" MODIFIED="1427512679343" TEXT="Bundles for files, packages, services and commands"/>
+<node CREATED="1427512682014" ID="ID_624431911" MODIFIED="1427512701438" TEXT="Maybe also edit_line(s) bundles"/>
+</node>
+<node CREATED="1427512740991" ID="ID_756717323" MODIFIED="1427514276752" TEXT="Programming Language">
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="xmag"/>
+</node>
+</node>
+<node CREATED="1427512753211" FOLDED="true" ID="ID_1528087126" MODIFIED="1427514311986" POSITION="left" TEXT="Desicion">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="xmag"/>
+<icon BUILTIN="attach"/>
+<node CREATED="1427512817034" ID="ID_322217483" MODIFIED="1427512842617" TEXT="Dashboard/Webapp Language">
+<font BOLD="true" NAME="SansSerif" SIZE="15"/>
+<node CREATED="1427512832700" ID="ID_152470690" MODIFIED="1427513002321" TEXT="GO">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1427512969181" ID="ID_1711910436" MODIFIED="1427513020710" TEXT="PRO">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1427513027522" ID="ID_567048346" MODIFIED="1427513095202" TEXT="Installing a go app is just dropping a binary somewhere and running it, no other external deps required"/>
+<node CREATED="1427513102313" ID="ID_1284970764" MODIFIED="1427513153247" TEXT="Stdlib has great HTTP lib (ssl, async, templating all integrated)"/>
+<node CREATED="1427513163652" ID="ID_348861548" MODIFIED="1427513168762" TEXT="Fast growing community"/>
+<node CREATED="1427513169691" ID="ID_248784505" MODIFIED="1427513177924" TEXT="Very fast execution and startup"/>
+<node CREATED="1427513529046" ID="ID_167086385" MODIFIED="1427513538151" TEXT="Goroutines for concurrency"/>
+<node CREATED="1427513767962" ID="ID_61971810" MODIFIED="1427513785217" TEXT="Project management with the simple go tools is intuitive and damn simple"/>
+</node>
+<node CREATED="1427512972826" ID="ID_984467666" MODIFIED="1427513022157" TEXT="CON">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1427513268437" ID="ID_1103044659" MODIFIED="1427513299903" TEXT="More verbose than Clojure because it&apos;s an imperative language with OO features"/>
+<node CREATED="1427513540328" ID="ID_1365104921" MODIFIED="1427513602026" TEXT="Not so easy/simple to use parallel execution support ">
+<icon BUILTIN="help"/>
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="xmag"/>
+</node>
+</node>
+</node>
+<node CREATED="1427512965349" ID="ID_783742713" MODIFIED="1427513004447" TEXT="Clojure">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1427512969181" ID="ID_1136644655" MODIFIED="1427513023238" TEXT="PRO">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1427513327503" ID="ID_1326134522" MODIFIED="1427513354947" TEXT="Still kinda easy to setup, only needs a JRE and would also run almost anywhere"/>
+<node CREATED="1427513356456" ID="ID_623490199" MODIFIED="1427513405227" TEXT="Functional with LISP dialect, thus very concise, easy to read and reason about"/>
+<node CREATED="1427513406258" ID="ID_1999485451" MODIFIED="1427513419008" TEXT="Immutability"/>
+<node CREATED="1427513420695" ID="ID_313138112" MODIFIED="1427513433515" TEXT="All the Java world is available">
+<node CREATED="1427513434286" ID="ID_1650519793" MODIFIED="1427513934632" TEXT="Interop introduces mutability though"/>
+</node>
+<node CREATED="1427513482698" ID="ID_1926315084" MODIFIED="1427513507042" TEXT="Great concurrency support"/>
+</node>
+<node CREATED="1427512972826" ID="ID_864307571" MODIFIED="1427513023952" TEXT="CON">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1427513306303" ID="ID_1425689895" MODIFIED="1427513315651" TEXT="Requires a JRE"/>
+<node CREATED="1427513316872" ID="ID_880085261" MODIFIED="1427513324874" TEXT="Slower startup"/>
+<node CREATED="1427513791170" ID="ID_233687424" MODIFIED="1427513824857" TEXT="Project management not really easy, though leiningen helps with that but it&apos;s nothing compared to go&apos;s way"/>
+</node>
+<node CREATED="1427513878472" ID="ID_1771951846" MODIFIED="1427513890623" TEXT="Neutral">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node CREATED="1427513891830" ID="ID_1820451057" MODIFIED="1427513914947" TEXT="Javainterop is nice but also kind of not so nice because">
+<node CREATED="1427513916129" ID="ID_1339511838" MODIFIED="1427513950830" TEXT="Mutability"/>
+<node CREATED="1427513952083" ID="ID_1061313104" MODIFIED="1427513976445" TEXT="Readability somehow suffers"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1427513667488" ID="ID_1487696001" MODIFIED="1427514954782" POSITION="right" TEXT="TODO">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="list"/>
+<node CREATED="1427513684151" ID="ID_1051033892" MODIFIED="1427513694844" TEXT="Look what libs GO provides for">
+<node CREATED="1427513695590" ID="ID_1821090421" MODIFIED="1427513711859" TEXT="PostgreSQL interaction"/>
+<node CREATED="1427513722723" ID="ID_1301727377" MODIFIED="1427513741033" TEXT="Other concurrency/parallel features"/>
+</node>
+<node CREATED="1427514956223" ID="ID_740594379" MODIFIED="1427514991472" TEXT="Take a look again what purpose the handle attribute has">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1427513997366" FOLDED="true" ID="ID_478969048" MODIFIED="1427514312638" POSITION="left" TEXT="Notes">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="idea"/>
+<icon BUILTIN="info"/>
+<node CREATED="1427514031549" ID="ID_759654591" MODIFIED="1427514250982" TEXT="What are the community needs/requirements?">
+<font BOLD="true" NAME="SansSerif" SIZE="13"/>
+<node CREATED="1427514072351" ID="ID_785511345" MODIFIED="1427514084703" TEXT="Simple and fast to setup application"/>
+<node CREATED="1427514085852" ID="ID_371477143" MODIFIED="1427514097709" TEXT="Easy accessible (to learn) tools"/>
+<node CREATED="1427514100787" ID="ID_476928038" MODIFIED="1427514135627" TEXT="Quick results">
+<node CREATED="1427514145847" ID="ID_1267039763" MODIFIED="1427514180809" TEXT="One might argue about that how this could worsen security and cause bad and brittle infrastructures"/>
+</node>
+</node>
+</node>
+<node CREATED="1427514305852" ID="ID_746916168" MODIFIED="1427514357986" POSITION="right" TEXT="Data">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<icon BUILTIN="info"/>
+<node CREATED="1427514392656" ID="ID_724278517" MODIFIED="1427514402143" TEXT="Host info reports">
+<node CREATED="1427514421147" ID="ID_193304044" MODIFIED="1427514425006" TEXT="General host info"/>
+<node CREATED="1427514426075" ID="ID_1109883304" MODIFIED="1427514430554" TEXT="Network related info">
+<node CREATED="1427514431221" ID="ID_1786171693" MODIFIED="1427514433238" TEXT="Interfaces"/>
+<node CREATED="1427514435078" ID="ID_427804215" MODIFIED="1427514451335" TEXT="Listening ports (TCP only)"/>
+</node>
+<node CREATED="1427514455240" ID="ID_1073228797" MODIFIED="1427514460479" TEXT="Installed software"/>
+</node>
+<node CREATED="1427514403498" ID="ID_755302461" MODIFIED="1427514405926" TEXT="Classes">
+<node CREATED="1427514464674" ID="ID_1590152403" MODIFIED="1427514476636" TEXT="Provide">
+<node CREATED="1427514477802" ID="ID_277683287" MODIFIED="1427514487015" TEXT="Info of host via hard classes"/>
+<node CREATED="1427514492836" ID="ID_184187498" MODIFIED="1427514552996" TEXT="Soft classes">
+<arrowlink COLOR="#b0b0b0" DESTINATION="ID_1473555828" ENDARROW="Default" ENDINCLINATION="120;0;" ID="Arrow_ID_1236904590" STARTARROW="None" STARTINCLINATION="120;0;"/>
+</node>
+</node>
+</node>
+<node CREATED="1427514514834" ID="ID_1473555828" MODIFIED="1427514541472" TEXT="Promise outcomes">
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1473555828" ENDARROW="Default" ENDINCLINATION="120;0;" ID="Arrow_ID_1236904590" SOURCE="ID_184187498" STARTARROW="None" STARTINCLINATION="120;0;"/>
+<node CREATED="1427514665290" ID="ID_1232051184" MODIFIED="1427514699039" TEXT="Promiser"/>
+<node CREATED="1427514700060" ID="ID_1892161947" MODIFIED="1427514702653" TEXT="Promisee"/>
+<node CREATED="1427514703693" ID="ID_1638090791" MODIFIED="1427514707174" TEXT="Outcome">
+<node CREATED="1427514707745" ID="ID_749077929" MODIFIED="1427514709059" TEXT="kept"/>
+<node CREATED="1427514709961" ID="ID_1638886884" MODIFIED="1427514712071" TEXT="repaired"/>
+<node CREATED="1427514713472" ID="ID_1793371148" MODIFIED="1427514714563" TEXT="failed"/>
+</node>
+<node CREATED="1427514938740" ID="ID_210455210" MODIFIED="1427514946397" TEXT="Handle">
+<icon BUILTIN="xmag"/>
+</node>
+</node>
+</node>
+</node>
+</map>
